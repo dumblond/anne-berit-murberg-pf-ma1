9e410cd0c7459c7010ee01a2c38db1faae6e1db7
Anne-Berit <anne.berit.murberg@hammerfest.kommune.no>
2020-09-23 14:20:45 +0200

    Finished product

---
+++ script.js
@@ -0,0 +1,37 @@
+// question 1
+
+var firstVar;
+
+// guestion 2
+
+var name ="Anne-Berit";
+
+// question 3
+
+var number = 13;
+
+// question 4
+
+var division = 20 / 5 ;
+
+// question 5
+
+var type = typeof "frog";
+
+// question 6
+
+var orderHasShipped = true;
+
+// question 7
+
+if (orderHasShipped === true) {
+    console.log("true");
+} else {
+    console.log("false");
+}
+
+// question 8
+
+for (var i = 0; i <= 9; i++) {
+    console.log(i);
+}
